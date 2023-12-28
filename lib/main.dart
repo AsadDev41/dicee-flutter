7aa97e44a4978bed9811e04ca3372d6215724baf
Asad <stormyboye5987@gmail.com>
2023-12-28 04:03:14 -0800

    dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<DicePage> {
+  int leftDicenumber = 1;
+  int rightDicenumber = 1;
+  void chageDiceface() {
+    rightDicenumber = Random().nextInt(6) + 1;
+    leftDicenumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  chageDiceface();
+                });
+              },
+              child: Image.asset('images/dice$leftDicenumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  chageDiceface();
+                });
+              },
+              child: Image.asset('images/dice$rightDicenumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
